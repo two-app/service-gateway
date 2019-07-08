b4fcc7d130e5ebf030b1264dc48bd8be44791946
gerryfletch <1997gerry@gmail.com>
2019-07-08 21:33:42 +0100

    Enabled Zuul routing for authentication service.

--- src/main/java/com/two/servicegateway/ServiceGatewayApplication.java
+++ src/main/java/com/two/servicegateway/ServiceGatewayApplication.java
@@ -3,8 +3,10 @@ package com.two.servicegateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
 public class ServiceGatewayApplication {
 
