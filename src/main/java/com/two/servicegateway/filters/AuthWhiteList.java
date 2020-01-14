9f3add9eac1dd33025b4c2087d7ab13fb8ff7885
gerryfletch <1997Gerry@gmail.com>
2020-01-14 22:42:58 +0000

    Removed static and used private

--- src/main/java/com/two/servicegateway/filters/AuthWhiteList.java
+++ src/main/java/com/two/servicegateway/filters/AuthWhiteList.java
@@ -6,12 +6,12 @@ import java.util.Set;
 
 @Component
 public class AuthWhiteList {
-    public Set<Route> ROUTES = Set.of(
+    private Set<Route> routes = Set.of(
             Route.POST("/self"),
             Route.POST("/login")
     );
 
     public boolean contains(String path, String method) {
-        return ROUTES.contains(Route.OF(path, method));
+        return routes.contains(Route.OF(path, method));
     }
 }
